d345e83430cd34bcb4f934ca35512cc1108c3015
Ragarnoy <tiagolernould@gmail.com>
2020-10-20 17:49:04 +0200

    Finish naive rust interfacing. Impl naive move.

--- src/goban.rs
+++ src/goban.rs
@@ -1,6 +1,12 @@
 use std::fmt;
-use crate::{BWIDTH};
 use std::fmt::Formatter;
+
+const EMPTY: u8 = 0;
+const BLACK: u8 = 1;
+const WHITE: u8 = 2;
+const BWIDTH: u8 = 19;
+const BHEIGHT: u8 = 19;
+const BSIZE: u16 = 361;
 
 #[derive(Clone, Debug)]
 pub struct Goban
@@ -11,13 +17,14 @@ pub struct Goban
 
 impl fmt::Display for Goban {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let mut ret: String = String::with_capacity(500);
+        let mut ret: String = String::with_capacity(400);
+        ret.push('\n');
         for (i, x) in self.grid.iter().enumerate()
         {
-            ret.push_str(&*format!("{} ", x));
             if i % 19 == 0 {
                 ret.push('\n');
             }
+            ret.push_str(&*format!("{} ", x));
         }
         write!(f, "{}", ret)
     }
@@ -41,6 +48,32 @@ impl Goban
             p_color
         }
     }
+
+    pub fn get_move(&self, pos: (usize, usize), dir: Move) -> u8
+    {
+        match dir {
+            Move::Up => self[(pos.0, pos.1 - 1)],
+            Move::UpLeft => self[(pos.0 - 1, pos.1 - 1)],
+            Move::UpRight => self[(pos.0 + 1, pos.1 - 1)],
+            Move::Left => self[(pos.0 - 1, pos.1)],
+            Move::Right => self[(pos.0 + 1, pos.1)],
+            Move::DownLeft => self[(pos.0 - 1, pos.1 + 1)],
+            Move::DownRight => self[(pos.0 + 1, pos.1 + 1)],
+            Move::Down => self[(pos.0, pos.1 + 1)],
+        }
+    }
+
+    pub fn list_pawns(&self) -> Vec<(u8, u8)>
+    {
+        self.grid.iter().enumerate().filter(|(_, i)| **i == self.p_color).map(|(pc, _)| ((pc % 19) as u8, (pc / 19) as u8)).collect()
+    }
+
+    pub fn heuristics(self) -> u32
+    {
+        self.grid.iter().filter(|i| **i == self.p_color).fold(0, |acc, x| acc + x);
+
+        unimplemented!()
+    }
 }
 
 pub enum Move
@@ -57,8 +90,24 @@ pub enum Move
 
 #[cfg(test)]
 mod tests {
+    use crate::goban::{Goban, BLACK, WHITE};
+
     #[test]
-    fn display() {
-        assert_eq!(2 + 2, 4);
+    fn count_pawns() {
+        let mut input: Vec<u8> = vec![0; 361];
+
+        input[4] = WHITE;
+        input[6] = WHITE;
+        input[0] = BLACK;
+        input[1] = BLACK;
+        input[2] = BLACK;
+        input[3] = BLACK;
+        input[18] = BLACK;
+        input[20] = BLACK;
+        input[21] = BLACK;
+        input[38] = BLACK;
+        input[180] = BLACK;
+        let go: Goban = Goban::new(input, BLACK);
+        assert_eq!(go.list_pawns(), vec![(0,0), (1, 0), (2, 0), (3, 0), (18, 0), (1, 1), (2, 1), (0, 2), (9, 9)]);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,22 @@ mod goban;
 
 use numpy::{PyArray2};
 use pyo3::prelude::{pymodule, Py, PyModule, PyResult, Python};
-use pyo3::{IntoPy, exceptions};
-use pyo3::types::{PyTuple, PyBool, PyInt, PyLong};
+use pyo3::{exceptions};
+use pyo3::types::{PyBool};
 use goban::Goban;
-
-const EMPTY: u8 = 0;
-const WHITE: u8 = 2;
-const BLACK: u8 = 1;
-const BWIDTH: u8 = 19;
-const BHEIGHT: u8 = 19;
-const BSIZE: u16 = 361;
 
 #[pymodule]
 fn rust_ext(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "ret_coord")]
-    fn ret_coord(py: Python<'_>, goban: &PyArray2<u8>, p_color: u8, hint: &PyBool) -> PyResult<Py<PyTuple>> {
+    fn ret_coord(py: Python<'_>, goban: &PyArray2<u8>, p_color: u8, hint: &PyBool) -> PyResult<(u32, u32)> {
         let board = goban.to_vec()?;
         if board.len() != 361 {
             return Err(exceptions::PyTypeError::new_err(format!("Fatal Rust Error: Invalid board size (Expected 361, got {})", board.len())));
         }
         let goban: Goban = Goban::new(board, p_color);
         // Do my stuff
-        let ret: &PyTuple = PyTuple::new(py, [2, 1].iter()); //placeholder
-        Ok(ret.into_py(py))
+        let ret = (5, 6); //placeholder
+        Ok(ret)
     }
     #[pyfn(m, "debug_heuristic")]
     fn debug_heuristic(py: Python<'_>, goban: &PyArray2<u8>) -> PyResult<u32> {
