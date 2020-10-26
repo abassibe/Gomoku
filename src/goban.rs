d0b6da56732b7b2ab0b8ac45d6efe66310f8532d
Ragarnoy <tiagolernould@gmail.com>
2020-10-26 19:21:08 +0100

    (very) naive heuristic on alignment.

--- src/goban.rs
+++ src/goban.rs
@@ -24,7 +24,7 @@ impl fmt::Display for Goban {
             if i % 19 == 0 {
                 ret.push('\n');
             }
-            ret.push_str(&*format!("{} ", x));
+            ret.push_str(&*format!("{:?} ", x));
         }
         write!(f, "{}", ret)
     }
@@ -52,12 +52,12 @@ impl Goban
     pub fn get_pos(&self, pos: (usize, usize), dir: Move) -> Option<(usize, usize)>
     {
         match dir {
-            Move::Up => Some((pos.0, pos.1 - 1)),
-            Move::UpLeft =>Some((pos.0 - 1, pos.1 - 1)),
-            Move::UpRight => if pos.0 < 19 { Some((pos.0 + 1, pos.1 - 1)) } else { None },
-            Move::Left => Some((pos.0 - 1, pos.1)),
+            Move::Up => if pos.1 != 0 { Some((pos.0, pos.1 - 1)) } else { None },
+            Move::UpLeft => if pos.1 != 0 && pos.0 != 0 { Some((pos.0 - 1, pos.1 - 1)) } else { None },
+            Move::UpRight => if pos.1 != 0 && pos.0 < 19 { Some((pos.0 + 1, pos.1 - 1)) } else { None },
+            Move::Left => if pos.0 != 0 { Some((pos.0 - 1, pos.1)) } else { None },
             Move::Right => if pos.0 < 19 { Some((pos.0 + 1, pos.1)) } else { None },
-            Move::DownLeft => if pos.1 < 19 { Some((pos.0 - 1, pos.1 + 1)) } else { None },
+            Move::DownLeft => if pos.0 != 0 && pos.1 < 19 { Some((pos.0 - 1, pos.1 + 1)) } else { None },
             Move::DownRight => if pos.1 < 19 && pos.0 < 19 { Some((pos.0 + 1, pos.1 + 1)) } else { None },
             Move::Down => if pos.1 < 19 { Some((pos.0, pos.1 + 1)) } else { None },
         }
@@ -68,15 +68,17 @@ impl Goban
         self.grid.iter().enumerate().filter(|(_, i)| **i == self.p_color).map(|(pc, _)| (pc % 19, pc / 19)).collect()
     }
 
-    fn check_line(&self, mut pos: (usize, usize), dir: Move) -> u32
+    fn check_line(&self, pos: (usize, usize), dir: Move) -> u32
     {
-        let mut ret : u32 = 1;
+        let mut ret : u32 = 0;
         let mut next = self.get_pos(pos, dir);
 
-        while self[pos] == self.p_color && next.is_some()
+        println!("Checking...");
+        while next.is_some() && self[next.unwrap()] == self.p_color
         {
-            pos = self.get_pos(pos, dir).unwrap();
+            next = self.get_pos(next.unwrap(), dir);
             ret *= 10;
+            println!("In align ret= {:?}, pos= {:?}", ret, pos);
         }
 	    ret
     }
@@ -84,20 +86,18 @@ impl Goban
     pub fn heuristics(&self) -> u32
     {
         let mut ret: u32 = 0;
-        let mut m = Move::Down;
         let pawns: Vec<(usize, usize)> = self.list_pawns();
 
         for (x, y) in pawns {
             for val in [Move::Down, Move::DownRight, Move::DownLeft, Move::Right,
                                         Move::Left, Move::Up, Move::UpRight, Move::UpLeft].iter()
             {
-                println!("Pos: {:?} dir:{:?}", (x, y), m);
-                ret += self.check_line((x, y), m)
+                println!("Pos: {:?} dir:{:?}", (x, y), val);
+                ret += self.check_line((x, y), *val)
             }
         }
         ret
-        // TODO Iterator or for loop like check surroundings and deduce direction then check if several aligned to optimize the amount of reads.
-        // TODO Node shit
+        // TODO Flag pawns that were found in alignment
     }
 }
 
@@ -148,7 +148,7 @@ mod tests {
         input[4] = BLACK;
         let go: Goban = Goban::new(input, BLACK);
 	    let ret = go.heuristics();
-        println!("{} {}", go, ret);
-	    assert_eq!(ret, 1000000);
+        println!("{}\n{:?}", go, ret);
+	    assert_eq!(ret, 10000);
     }
 }
