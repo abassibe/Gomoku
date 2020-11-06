d4be1c46c65df34464418029037b484bc449f794
Valentin Sporer <v@sporer.fr>
2020-11-06 17:43:13 +0100

    Move trait's implementation to the file's bottom

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -25,6 +25,175 @@ pub struct BitBoard {
     b: [u128; 3]
 }
 
+// ----------------------------------------
+// Homemade methods implemented on BitBoard
+// ----------------------------------------
+impl BitBoard {
+    const MOVE_UP_DOWN_SHIFT_VALUE: u32 = 19;
+
+    // ------------
+    // Constructors
+    // ------------
+    pub fn new(one: u128, two: u128, three: u128) -> Self {
+        Self { b: [one, two, three] }
+    }
+
+    pub fn from_array(from: [u128; 3]) -> Self { 
+        Self { b: from }
+    }
+
+    pub fn full() -> Self {
+        !Self::default()
+    }
+
+    pub fn empty() -> Self {
+        Self::default()
+    }
+
+    // -------------
+    // Tests methods
+    // -------------
+    /// Returns `true` if **every** bits are set to 1 in the bitboard.
+    /// Returns `false` otherwise.
+    pub fn is_full(&self) -> bool {
+        for x in &self.b {
+            if *x != u128::MAX {
+                return false
+            }
+        }
+
+        true
+    }
+
+    /// Returns `true` if **no** bits are set to 1 in the bitboard.
+    /// Returns `false` otherwise.
+    pub fn is_empty(&self) -> bool {
+        for x in &self.b {
+            if *x != 0 {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    // ---------------------------------
+    // Computation's method for BitBoard
+    // ---------------------------------
+    pub fn compute_to_isize<F: Fn(&Self) -> isize>(&self, f: F) -> isize {
+        f(self)
+    }
+
+    // ---------------------------------------
+    // Implementation of bitshift for BitBoard
+    // ---------------------------------------
+    #[inline]
+    fn shift_left(&self, by: usize) -> Self {
+        let bits = self.b;
+        let max_index = bits.len() - 1;
+        let mut new_bits: [u128; 3] = [0, 0, 0];
+
+        if by >= BITS_IN_U128 * (max_index + 1) {
+            return Self::default();
+        }
+
+        let inner_lshift = by % BITS_IN_U128;
+        let inner_rshift = BITS_IN_U128 - inner_lshift;
+        let value_off = by / BITS_IN_U128;
+        for (dest_i, src_i) in (0..=(max_index - value_off)).rev().zip((0..=max_index).rev()) {
+            if src_i < max_index && inner_rshift < BITS_IN_U128 {
+                new_bits[dest_i] = bits[src_i + 1] >> inner_rshift
+            }
+            new_bits[dest_i] |= bits[src_i] << inner_lshift;
+        }
+
+        Self {
+            b: new_bits
+        }
+    }
+
+    #[inline]
+    fn shift_right(&self, by: usize) -> Self {
+        let bits = self.b;
+        let max_index = bits.len() - 1;
+        let mut new_bits: [u128; 3] = [0, 0, 0];
+
+        if by >= BITS_IN_U128 * (max_index + 1) {
+            return Self::default();
+        }
+
+        let inner_rshift = by % BITS_IN_U128;
+        let inner_lshift = BITS_IN_U128 - inner_rshift;
+        let value_off = by / BITS_IN_U128;
+        for (dest_i, src_i) in (value_off..=max_index).zip((0..=max_index)) {
+            if src_i > usize::MIN && inner_lshift < BITS_IN_U128 {
+                new_bits[dest_i] = bits[src_i - 1] << inner_lshift
+            }
+            new_bits[dest_i] |= bits[src_i] >> inner_rshift;
+        }
+
+        Self {
+            b: new_bits
+        }
+    }
+
+    // TODO: Missing doc here
+    fn shift_direction(&self, direction: Direction) -> Self {
+        let board = *self;
+        match direction {
+            Direction::N => board << Self::MOVE_UP_DOWN_SHIFT_VALUE,
+            Direction::S => board >> Self::MOVE_UP_DOWN_SHIFT_VALUE,
+            Direction::E => board >> 1,
+            Direction::W => board << 1,
+            Direction::NE => board << Self::MOVE_UP_DOWN_SHIFT_VALUE - 1,
+            Direction::NW => board << Self::MOVE_UP_DOWN_SHIFT_VALUE + 1,
+            Direction::SE => board >> Self::MOVE_UP_DOWN_SHIFT_VALUE + 1,
+            Direction::SW => board >> Self::MOVE_UP_DOWN_SHIFT_VALUE - 1,
+            Direction::All => unimplemented!("You MUST not use Direction::All with this method")
+        }
+    }
+
+    // --------------------
+    // Methods for dilation
+    // --------------------
+    /// This method should remain private.
+    /// Use the operator `+` instead.
+    #[inline]
+    fn dilate(&self, dir: Direction) -> Self {
+        match dir {
+            Direction::All => {
+                let mut result = *self;
+                for d in DirectionIterator::new() {
+                    // TODO: Replace this `= result` with a `|=` when OrAssign will be implemented
+                    result = result | self.shift_direction(d);
+                }
+                result
+            },
+            d => *self | self.shift_direction(d)
+        }
+    }
+
+    // -------------------
+    // Methods for erosion
+    // -------------------
+    /// This method should remain private.
+    /// Use the operator `-` instead.
+    #[inline]
+    fn erode(&self, dir: Direction) -> Self {
+        match dir {
+            Direction::All => {
+                let mut result = *self;
+                for d in DirectionIterator::new() {
+                    // TODO: Replace this `= result` with a `&=` when OrAssign will be implemented
+                    result = result & self.shift_direction(d);
+                }
+                result
+            },
+            d => *self & self.shift_direction(d)
+        }
+    }
+}
+
 // ----------------------------------------------
 // Implementation of trait's methods on BitBoard.
 // ----------------------------------------------
@@ -198,171 +367,7 @@ impl Not for &BitBoard {
 }
 
 
-// ----------------------------------------
-// Homemade methods implemented on BitBoard
-// ----------------------------------------
-impl BitBoard {
-    const MOVE_UP_DOWN_SHIFT_VALUE: u32 = 19;
+// Perform a dilation on a `BitBoard` using the provided `Direction`
+// impl Add for BitBoard {
 
-    // ------------
-    // Constructors
-    // ------------
-    pub fn new(one: u128, two: u128, three: u128) -> Self {
-        Self { b: [one, two, three] }
-    }
-
-    pub fn from_array(from: [u128; 3]) -> Self { 
-        Self { b: from }
-    }
-
-    pub fn full() -> Self {
-        !Self::default()
-    }
-
-    pub fn empty() -> Self {
-        Self::default()
-    }
-
-    // -------------
-    // Tests methods
-    // -------------
-    /// Returns `true` if **every** bits are set to 1 in the bitboard.
-    /// Returns `false` otherwise.
-    pub fn is_full(&self) -> bool {
-        for x in &self.b {
-            if *x != u128::MAX {
-                return false
-            }
-        }
-
-        true
-    }
-
-    /// Returns `true` if **no** bits are set to 1 in the bitboard.
-    /// Returns `false` otherwise.
-    pub fn is_empty(&self) -> bool {
-        for x in &self.b {
-            if *x != 0 {
-                return false;
-            }
-        }
-
-        true
-    }
-
-    // ---------------------------------
-    // Computation's method for BitBoard
-    // ---------------------------------
-    pub fn compute_to_isize<F: Fn(&Self) -> isize>(&self, f: F) -> isize {
-        f(self)
-    }
-
-    // ---------------------------------------
-    // Implementation of bitshift for BitBoard
-    // ---------------------------------------
-    #[inline]
-    fn shift_left(&self, by: usize) -> Self {
-        let bits = self.b;
-        let max_index = bits.len() - 1;
-        let mut new_bits: [u128; 3] = [0, 0, 0];
-
-        if by >= BITS_IN_U128 * (max_index + 1) {
-            return Self::default();
-        }
-
-        let inner_lshift = by % BITS_IN_U128;
-        let inner_rshift = BITS_IN_U128 - inner_lshift;
-        let value_off = by / BITS_IN_U128;
-        for (dest_i, src_i) in (0..=(max_index - value_off)).rev().zip((0..=max_index).rev()) {
-            if src_i < max_index && inner_rshift < BITS_IN_U128 {
-                new_bits[dest_i] = bits[src_i + 1] >> inner_rshift
-            }
-            new_bits[dest_i] |= bits[src_i] << inner_lshift;
-        }
-
-        Self {
-            b: new_bits
-        }
-    }
-
-    #[inline]
-    fn shift_right(&self, by: usize) -> Self {
-        let bits = self.b;
-        let max_index = bits.len() - 1;
-        let mut new_bits: [u128; 3] = [0, 0, 0];
-
-        if by >= BITS_IN_U128 * (max_index + 1) {
-            return Self::default();
-        }
-
-        let inner_rshift = by % BITS_IN_U128;
-        let inner_lshift = BITS_IN_U128 - inner_rshift;
-        let value_off = by / BITS_IN_U128;
-        for (dest_i, src_i) in (value_off..=max_index).zip((0..=max_index)) {
-            if src_i > usize::MIN && inner_lshift < BITS_IN_U128 {
-                new_bits[dest_i] = bits[src_i - 1] << inner_lshift
-            }
-            new_bits[dest_i] |= bits[src_i] >> inner_rshift;
-        }
-
-        Self {
-            b: new_bits
-        }
-    }
-
-    // TODO: Missing doc here
-    fn shift_direction(&self, direction: Direction) -> Self {
-        let board = *self;
-        match direction {
-            Direction::N => board << Self::MOVE_UP_DOWN_SHIFT_VALUE,
-            Direction::S => board >> Self::MOVE_UP_DOWN_SHIFT_VALUE,
-            Direction::E => board >> 1,
-            Direction::W => board << 1,
-            Direction::NE => board << Self::MOVE_UP_DOWN_SHIFT_VALUE - 1,
-            Direction::NW => board << Self::MOVE_UP_DOWN_SHIFT_VALUE + 1,
-            Direction::SE => board >> Self::MOVE_UP_DOWN_SHIFT_VALUE + 1,
-            Direction::SW => board >> Self::MOVE_UP_DOWN_SHIFT_VALUE - 1,
-            Direction::All => unimplemented!("You MUST not use Direction::All with this method")
-        }
-    }
-
-    // --------------------
-    // Methods for dilation
-    // --------------------
-    /// This method should remain private.
-    /// Use the operator `+` instead.
-    #[inline]
-    fn dilate(&self, dir: Direction) -> Self {
-        match dir {
-            Direction::All => {
-                let mut result = *self;
-                for d in DirectionIterator::new() {
-                    // TODO: Replace this `= result` with a `|=` when OrAssign will be implemented
-                    result = result | self.shift_direction(d);
-                }
-                result
-            },
-            d => *self | self.shift_direction(d)
-        }
-    }
-
-    // -------------------
-    // Methods for erosion
-    // -------------------
-    /// This method should remain private.
-    /// Use the operator `-` instead.
-    #[inline]
-    fn erode(&self, dir: Direction) -> Self {
-        match dir {
-            Direction::All => {
-                let mut result = *self;
-                for d in DirectionIterator::new() {
-                    // TODO: Replace this `= result` with a `&=` when OrAssign will be implemented
-                    result = result & self.shift_direction(d);
-                }
-                result
-            },
-            d => *self & self.shift_direction(d)
-        }
-    }
-}
+// }
