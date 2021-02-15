f766fe92e2d1586135606a3e1b4b272be41efe15
Tiago Lernould <tiagolernould@gmail.com>
2021-02-15 16:57:49 +0100

    Receive array from python and convert to bitboard.

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,10 @@ mod algorithm;
 
 use numpy::{PyArray2};
 use pyo3::prelude::{pymodule, Py, PyModule, PyResult, Python};
-use pyo3::{exceptions};
+use pyo3::{exceptions, PyAny};
 use pyo3::types::{PyBool};
 use goban::Goban;
+use crate::bitboard::BitBoard;
 
 // Comment rajouter une fonction python sur rust
 // Simplement rajouter dans le block pymodule une fonction rust avec obligatoirement une instance Python<'_>, et si applicable un PyResult pour le retour
@@ -18,23 +19,37 @@ use goban::Goban;
 fn rust_ext(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "ret_coord")]
     fn ret_coord(py: Python<'_>, goban: &PyArray2<u8>, p_color: u8, hint: &PyBool) -> PyResult<(u32, u32)> {
-        let board = goban.to_vec()?;
+        let board:Vec<u8> = goban.to_vec()?;
         if board.len() != 361 {
             return Err(exceptions::PyTypeError::new_err(format!("Fatal Rust Error: Invalid board size (Expected 361, got {})", board.len())));
         }
         // Do my stuff
+        let mut strBoard: String = String::from_utf8(board).unwrap();
+        println!("{}", strBoard);
+        for i in 0..19 {
+            strBoard.insert((i + 1) * 19, '\n');
+        }
+        println!("{}", strBoard);
         let ret = (5, 6); //placeholder
         Ok(ret)
     }
-    #[pyfn(m, "debug_heuristic")]
-    fn debug_heuristic(py: Python<'_>, goban: &PyArray2<u8>) -> PyResult<u32> {
+    #[pyfn(m, "debug")]
+    fn debug(py: Python<'_>, goban: &PyArray2<u8>) -> PyResult<u8> {
         let board = goban.to_vec()?;
-        let mut ret: u32 = 0;
         if board.len() != 361 {
             return Err(exceptions::PyTypeError::new_err(format!("Fatal Rust Error: Invalid board size (Expected 361, got {})", board.len())));
         }
-        ret = 33;
-        Ok(ret)
+
+        let mut str_board: String = board.into_iter().enumerate().map(|(x, i)|{
+            if x % 19 == 0 {
+                "\n".to_owned() + &i.to_string()
+            }
+            else {
+                i.to_string()
+            }
+        }).collect::<String>();
+        println!("{}", BitBoard::from_str(&str_board));
+        Ok(1u8)
     }
     Ok(())
 }
