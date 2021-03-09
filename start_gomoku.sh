0215f8c22cb87d33d685c6a446c454a3fbc9d69b
arthur laine <arthur.laine99@gmail.com>
2021-03-09 13:00:16 +0100

    added some things inside init_gomoku_env, rust install is not lauchning if its already installed, you can say if you want to uninstall and then re install it, this one should work just fine, maybe just an issue with source /Users/arlaine/.cargo/env, but other than that I don't see why this install script version wouldn't work

--- start_gomoku.sh
+++ start_gomoku.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DYLIB=target/debug/librust_ext.dylib
-PYTHON=src/PyInterface/gomoku.py
+GOMOKU_PATH=src/PyInterface/gomoku.py
 maturin develop --release
 if test -f "$DYLIB"; then
     mv $DYLIB .
 fi
-venv/bin/python $PYTHON
+venv/bin/python $GOMOKU_PATH
 #maturin develop --release && mv target/debug/librust_ext.dylib . && bin/python3.9 src/PyInterface/gomoku.py
