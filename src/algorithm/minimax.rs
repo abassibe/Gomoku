9b90300aa61f2977086bbafef34725a538c2ca89
Tiago Lernould <tiagolernould@gmail.com>
2021-03-15 17:07:44 +0100

    Sort by estimate function that will go away. move node generator here

--- src/algorithm/minimax.rs
+++ src/algorithm/minimax.rs
@@ -55,7 +55,59 @@ impl Algorithm {
                 }
             }
         }
-
         candidate
+    }
+
+    fn sort_by_estimate(nodes: &mut Vec<Node>) {
+        for (i, node) in nodes.iter().enumerate() {
+            println!("(before) node[{:?}] estimation is : {:?}", i, node.get_item().get_estimation());
+        }
+        nodes.sort_unstable_by(|a, b| b.get_item().get_estimation().partial_cmp(&a.get_item().get_estimation()).unwrap());
+        for (i, node) in nodes.iter().enumerate() {
+            println!("(after) node[{:?}] estimation is : {:?}", i, node.get_item().get_estimation());
+        }
+    }
+
+    // TODO: Could be more efficient to calculate the score for each new node
+    // and then sort the resulting Vec<Node> according this score.
+    pub(crate) fn node_generator(&self, parent: &Node, maximazing: bool) -> Vec<Node> {
+        let parent_goban = parent.get_item();
+        let parent_player = parent_goban.get_player();
+        let parent_enemy = parent_goban.get_enemy();
+        let parent_player_captures = parent.get_player_captures();
+        let parent_enemy_captures = parent.get_opponent_captures();
+
+        // TODO: Investigate this call and its return value (especially for open 2).
+        let mut ret : Vec<Node> = self.get_potential_moves(parent)
+            .enumerate()
+            .iter()
+            .map(|b| {
+                let mut player_captures = parent_player_captures;
+                let mut enemy_captures = parent_enemy_captures;
+                let (player, enemy, is_players_move) =
+                    if maximazing {
+                        let player_with_move = parent_player | b;
+                        let captured_by_player = extract_captured_by_move(player_with_move, *parent_enemy, *b, &self.patterns);
+                        if captured_by_player.is_any() {
+                            player_captures += (captured_by_player.count_ones() / 2) as u8;
+                            (player_with_move, parent_enemy ^ &captured_by_player, true)
+                        } else {
+                            (player_with_move, *parent_enemy, true)
+                        }
+                    } else {
+                        let enemy_with_move = parent_enemy | b;
+                        let captured_by_enemy = extract_captured_by_move(enemy_with_move, *parent_player, *b, &self.patterns);
+                        if captured_by_enemy.is_any() {
+                            enemy_captures += (captured_by_enemy.count_ones() / 2) as u8;
+                            (parent_player ^ &captured_by_enemy, enemy_with_move, false)
+                        } else {
+                            (*parent_player, enemy_with_move, false)
+                        }
+                    };
+                Node::new(Goban::new_with_estimation(player, enemy), parent.get_depth() + 1, *b, is_players_move, player_captures, enemy_captures)
+            })
+            .collect();
+        Self::sort_by_estimate(&mut ret);
+        ret
     }
 }
