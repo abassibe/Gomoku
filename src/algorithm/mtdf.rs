f85c5a0f955409582b9aae6e1001fb67a7c2bba9
Tiago Lernould <tiagolernould@gmail.com>
2021-03-28 15:23:32 +0200

    Initial work on mtdf

--- src/algorithm/mtdf.rs
+++ src/algorithm/mtdf.rs
@@ -1,5 +1,14 @@
 use crate::algorithm::Algorithm;
+use crate::node::Node;
 
 impl Algorithm {
+    fn mtdf(&self, best_value: u32, depth: u32) -> Node {
+        let upperBound: u32 = u32::MAX;
+        let lowerBound: u32 = u32::MIN;
 
+        while lowerBound < upperBound {
+            
+        }
+        unimplemented!()
+    }
 }
