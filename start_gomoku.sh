78e71194e2a96f4b1193b5008c49d5d8ee271a07
Tiago Lernould <tiagolernould@gmail.com>
2021-04-12 18:02:52 +0200

    Modify shell script, try out cargo-make. create requirements file.

--- start_gomoku.sh
+++ start_gomoku.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DEFAULT_DYLIB_PATH=target/release/librust_ext.dylib
-DEFAULT_PYTHON_BIN=venv/bin/python3
+DEFAULT_PYTHON_BIN=./venv/bin/pyth
 
 if [[ $# -ge 1 && ($1 == "--help" || $1 == "-h") ]]; then
     echo -e "This script aims at building then running the project Gomoku.
@@ -14,7 +14,7 @@ if [[ $# -ge 1 && ($1 == "--help" || $1 == "-h") ]]; then
     No build has been performed, to build and run the project please rerun this script without the help option"
     exit 42
 elif [ $# -ge 1 ]; then
-    echo -e "Unknonw option $1 found.
+    echo -e "Unknown option $1 found.
     This script currently only supports '--help' and '-h' as options"
     exit 42
 fi
@@ -22,8 +22,10 @@ fi
 # Variable definitions
 if [[ -n $GOMOKU_PYTHON_BIN_PATH ]]; then
     PYTHON_BIN=$GOMOKU_PYTHON_BIN_PATH
-else
+    elif [ -f $DEFAULT_PYTHON_BIN ]; then
     PYTHON_BIN=$DEFAULT_PYTHON_BIN
+    else
+    PYTHON_BIN=$(which python3)
 fi
 
 if [[ -n $GOMOKU_RUST_LIB_PATH ]]; then
@@ -36,29 +38,37 @@ PYTHON=src/PyInterface/gomoku.py
 echo "Target dylib is expected to by found at the following path: $DYLIB"
 
 # Build
-maturin develop --release
-
 # Check if build is successful
-if [ $? -ne 0 ]; then
-    echo -e "\033[0;31mMaturin failed to build the project! Will NOT run the project\033[0m"
+if ! maturin develop --release; then
+    echo -e "\033[0;31mMaturin failed to build the project in a venv.\033[0m Will try to build without a virtual environment"
+    else
+      HAS_VENV=true
+    if ! pip3 install -r requirements.txt; then
+    echo -e "\033[0;31mCould not install requirements. Cannot build project\033[0m"
     exit 42
+    else
+      HAS_VENV=false
+    fi
 fi
 
 # Move the generated lib if it exists
 if test -f "$DYLIB"; then
-    echo -e "A new version of the dylib has been found at $DYLIB.\nCopying into current working directory $PWD"
-    mv $DYLIB .
+  if [ $HAS_VENV ]; then
+  echo -e "A new version of the dylib has been found at $DYLIB.\nMoving into current working directory $PWD"
+  mv $DYLIB .
+  else
+  echo -e "A new version of the dylib has been found at $DYLIB.\nMoving and renaming into appropriate directory src/PyInterface"
+  mv $DYLIB src/PyInterface/.
+  fi
 
     STATUS_CODE=$?
     # 0 is status code for success.
     # 2 is the status code returned by mv when the source and the target are the same (so not really an error we want to handle).
     if [ $STATUS_CODE -ne 0 ] && [ $STATUS_CODE -ne 2 ]; then
-        echo -e "\033[0;31mAn error occured while trying to move $DYLIB to $PWD. Will NOT run the project\033[0m"
+        echo -e "\033[0;31mAn error occurred while trying to move $DYLIB. Will NOT run the project\033[0m"
         exit 42
     fi
 fi
 
 # Run the project
 $PYTHON_BIN $PYTHON
-
-#maturin develop --release && mv target/debug/librust_ext.dylib . && bin/python3.9 src/PyInterface/gomoku.py
